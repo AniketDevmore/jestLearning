bb83e48c14e5bc0706bd1b185a193a8fad3f9d4f
Aniket Devmore <aniketdevmore1008@gmail.com>
2025-01-07 23:47:04 +0530

    replace vite app with create react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
-// import React from "react";
-// import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders without error", () => {
-
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
